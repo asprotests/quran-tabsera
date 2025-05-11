f3ba4de17851bd8a8bb22b9600b27967336c0321
Abdulrahman Binaspro <binaspro@gmail.com>
2025-05-11 12:34:45 +0300

    update

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,22 @@
 // api/proxy.js
 
 export default async function handler(req, res) {
-    const { from, to } = req.query;
-  
-    try {
-      const response = await fetch(`http://tabsera.com:8585/quran-teacher-report/report?from=${from}&to=${to}`);
-      
-      if (!response.ok) {
-        const error = await response.text();
-        return res.status(response.status).json({ error });
-      }
-  
-      const data = await response.json();
-      res.status(200).json(data);
-    } catch (err) {
-      console.error("Proxy error:", err.message);
-      res.status(500).json({ error: 'Proxy server error' });
+  const { from, to, gender } = req.query;
+
+  try {
+    const response = await fetch(
+      `http://tabsera.com:8585/quran-teacher-report/report?from=${from}&to=${to}&gender=${gender}`
+    );
+
+    if (!response.ok) {
+      const error = await response.text();
+      return res.status(response.status).json({ error });
     }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    console.error("Proxy error:", err.message);
+    res.status(500).json({ error: "Proxy server error" });
   }
-  
+}
